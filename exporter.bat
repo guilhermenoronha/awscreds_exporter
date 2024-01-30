831c4de71a58f7d9d937afdb0cf56d6ec46f006c
Guilherme Noronha <guilherme.francis@totvs.com.br>
2024-01-30 08:34:18 -0300

    enhanced script

--- exporter.bat
+++ exporter.bat
@@ -4,8 +4,8 @@ REM Command to export AWS variables to global environment
 
 SET JSON_FILE=%1
 
-FOR /F "tokens=*" %%g IN ('jq.exe -r .aws_access_key_id !JSON_FILE!') do (SETX AWS_ACCESS_KEY_ID "%%g")
-FOR /F "tokens=*" %%g IN ('jq.exe -r .aws_secret_access_key !JSON_FILE!') do (SETX AWS_SECRET_ACCESS_KEY "%%g")
-FOR /F "tokens=*" %%g IN ('jq.exe -r .aws_session_token !JSON_FILE!') do (SETX AWS_SESSION_TOKEN "%%g")
+FOR /F "tokens=*" %%g IN ('jq.exe -r .aws_access_key_id !JSON_FILE!') do (aws configure set aws_access_key_id "%%g")
+FOR /F "tokens=*" %%g IN ('jq.exe -r .aws_secret_access_key !JSON_FILE!') do (aws configure set aws_secret_access_key "%%g")
+FOR /F "tokens=*" %%g IN ('jq.exe -r .aws_session_token !JSON_FILE!') do (aws configure set aws_session_token "%%g")
 
 endlocal
